b56f26384dad45f9ed27711aede322891250eedb
何鸿涛 <3401611059@qq.com>
2024-12-08 17:18:12 +0800

    项目初始化

---
+++ xiaohongshu-auth/src/test/java/com/hht/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hht.xiaohongshu.auth;
+
+import com.hht.xiaohongshu.auth.domain.dataobject.UserDO;
+import com.hht.xiaohongshu.auth.domain.mapper.UserDOMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class XiaohongshuAuthApplicationTests {
+
+
+
+}
